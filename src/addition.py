ae080214f203ebdfc4c3ba8b4e034b82ef389474
LR2709 <146945884+LR2709@users.noreply.github.com>
2023-10-04 17:02:17 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -8,3 +8,11 @@ def test_add():
     assert add(1, -1) == 0
 
 # This is the first edit
+
+# This is a new function
+def multi(a,b):
+    return a*b
+
+def test_mul(a,b)
+assert multi(2,4) == 8
+assert multi(10,30) == 300
